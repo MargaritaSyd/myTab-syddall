9157cf69aa7dd7dead2b76b6658901d505a33a53
MargaritaSyd <margarita.syddall@gmail.com>
2022-02-07 16:58:06 -0300

    incorporando Flatlist

--- App.js
+++ App.js
@@ -1,14 +1,65 @@
+import { useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [textInput , setTextInput] = useState('');
+  const [itemList , setItemList] = useState([]);
+
+  const handleChangeText = (text) => {
+    setTextInput(text)
+  }
+
+  const handleOnPress = () => {
+    setTextInput('')
+    setItemList([
+      ...itemList, {
+        value: textInput,
+        id: Math.random().toString(),
+      },
+    ])
+  }
+
+ // console.log(textInput)
+ console.log(itemList)
+
   return (
     <View style={styles.container}>
       <Text>Agregá un nuevo item a tu inventario</Text>
       <View style={styles.inputContainer}>
-      <TextInput style={styles.inputStyle} />
-      <Button title="Add item" />
+      
+      <TextInput 
+        style={styles.inputStyle} 
+        placeholder='Marca'
+        onChangeText={handleChangeText}
+        value={textInput}
+      />
+      <Button  
+        onPress={handleOnPress}
+        title="Add item"
+      />
       </View>
+      {/* <View>
+        {itemList.map(item => (
+          <View key={item.id}>
+            <Text> {item.value} </Text>
+          </View>
+        ))
+        }
+      </View> */}
+      <FlatList 
+        data={itemList}
+        renderItem={({item}) => (
+          <View  style={styles.item}>
+            <Text> {item.value} </Text>
+          </View>
+
+        )}
+
+     // keyExtractor={ item => item.id}
+      /> 
+
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -21,10 +72,17 @@ const styles = StyleSheet.create({
   inputStyle: {
     borderBottomColor: "black",
     borderBottomWidth: 1,
-    flex: 1
+    flex: 1,
+    marginRight: 20
     
   },
   inputContainer: {
-    flexDirection: "row"
+  flexDirection: "row"
+  },
+  item: {
+    padding: 20,
+    marginVertical: 20,
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
