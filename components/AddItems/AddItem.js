6c4799def0acc37e69e901642960759962ed8f04
MargaritaSyd <margarita.syddall@gmail.com>
2022-03-01 16:25:47 -0300

    keyboardAvoidingView ios vs android

--- components/AddItems/AddItem.js
+++ components/AddItems/AddItem.js
@@ -1,32 +1,38 @@
 import React from "react";
-import { View, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import { View, TouchableWithoutFeedback, Keyboard , KeyboardAvoidingView} from 'react-native';
 import styles from "./styles";
 import Button from "../Button";
 import Input from '../Input'
+import { KeyboardAvoidingView } from "react-native-web";
 
 function AddItem({textInput, stockInput, handleChangeText, handleChangeStock, handleOnPress}){ 
 
     return(
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inputContainer}>
-      
-        <Input 
-          placeholder='Marca'
-          onChangeText={handleChangeText}
-          value={textInput}
-        />
-        <Input 
-          placeholder='stock'
-          onChangeText={handleChangeStock}
-          value={stockInput}
-          maxLength={4}
-        />
-        <Button  
-          onPress={handleOnPress}
-          title="Add item"
-        />
-        </View>
-      </TouchableWithoutFeedback>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.containerInputAddItem}
+      >  
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.inputContainer}>
+        
+          <Input 
+            placeholder='Marca'
+            onChangeText={handleChangeText}
+            value={textInput}
+          />
+          <Input 
+            placeholder='stock'
+            onChangeText={handleChangeStock}
+            value={stockInput}
+            maxLength={4}
+          />
+          <Button  
+            onPress={handleOnPress}
+            title="Add item"
+          />
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>  
     )
 }
 
